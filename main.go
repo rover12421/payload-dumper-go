dd6971f930d15e3a1c7c4f43c12bc3a6b5252288
EricShui <ericshui@foxmail.com>
2021-05-09 16:17:57 +0800

    Add files via upload
    del unzip *.zip func , mv create targetDirectory to payload.go

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	list := flag.Bool("l", false, "Show list of partitions in payload.bin")
-	partitions := flag.String("p", "", "Dump only selected partitions")
+	partitions := flag.String("p", "", "Dump only selected partitions ,each 10 minutes dump files include one folder")
 	flag.Parse()
 
 	if flag.NArg() == 0 {
@@ -62,16 +62,16 @@ func main() {
 	}
 
 	payloadBin := filename
-	if strings.HasSuffix(filename, ".zip") {
-		fmt.Println("Please wait while extracting payload.bin from the archive.")
-		payloadBin = extractPayloadBin(filename)
-		if payloadBin == "" {
-			log.Fatal("Failed to extract payload.bin from the archive.")
-		} else {
-			defer os.Remove(payloadBin)
-		}
-	}
-	fmt.Printf("payload.bin: %s\n", payloadBin)
+	// if strings.HasSuffix(filename, ".zip") {
+	// 	fmt.Println("Please wait while extracting payload.bin from the archive.")
+	// 	payloadBin = extractPayloadBin(filename)
+	// 	if payloadBin == "" {
+	// 		log.Fatal("Failed to extract payload.bin from the archive.")
+	// 	} else {
+	// 		defer os.Remove(payloadBin)
+	// 	}
+	// }
+	// fmt.Printf("payload.bin: %s\n", payloadBin)
 
 	payload := NewPayload(payloadBin)
 	if err := payload.Open(); err != nil {
@@ -84,10 +84,11 @@ func main() {
 	}
 
 	now := time.Now()
-	targetDirectory := fmt.Sprintf("extracted_%d%02d%02d_%02d%02d%02d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
-	if err := os.Mkdir(targetDirectory, 0755); err != nil {
-		log.Fatal("Failed to create target directory")
-	}
+	targetDirectory := fmt.Sprintf("extracted_%d%02d%02d_%d%d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute()/10)
+
+
+
+
 	if *partitions != "" {
 		if err := payload.ExtractSelected(targetDirectory, strings.Split(*partitions, ",")); err != nil {
 			log.Fatal(err)
@@ -98,6 +99,7 @@ func main() {
 		}
 	}
 }
+
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage: %s [inputfile]\n", os.Args[0])
